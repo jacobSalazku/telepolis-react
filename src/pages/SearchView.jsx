2aeb71317bc8d59e36e3637b025654d3c7ea2aca
jacobSalazku <Cooljss10@gmail.com>
2023-09-04 23:09:54 +0200

    fetching movie api

--- src/pages/SearchView.jsx
+++ src/pages/SearchView.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 
+ // API_KEY = 'cb8b5baf6bfe0c3929a841b05a0f27e4'
+ // movies in theaters 'https://api.themoviedb.org/3/movie/now_playing
+// movie details example https://api.themoviedb.org/3/movie/{movie_id}
+// exapmle link searches ''
 const SearchView = ({ keyword , searchResults }) => {
 
   const title  = `You are searching for ${keyword}`
@@ -8,7 +12,7 @@ const SearchView = ({ keyword , searchResults }) => {
   return (
 
     <Hero text ={title}/>
-    
+
   )
 }
 
