33a9009ed1574bfee5e2abc4bb809820e2d523fb
jacobSalazku <Cooljss10@gmail.com>
2023-09-09 01:46:22 +0200

    searchview

---
+++ src/pages/SearchView.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Hero from '../Components/Hero';
+import MovieCard from '../Components/MovieCard';
+
+const SearchView = ({ keyword, searchResults }) => {
+  const title = `You are searching for ${keyword}`;
+  const notFound =`${keyword} is not found`
+  const resultHtml = searchResults.map((obj, i) => {
+    return <MovieCard movie={obj} key={i} />
+});
+ 
+  return (
+    <>
+      <Hero text={title} />
+        <div className='flex-row-center'>
+            { searchResults.length===0? <div> <h1>{notFound}</h1></div> : resultHtml }
+        </div>
+    </>
+  );
+};
+
+export default SearchView;
