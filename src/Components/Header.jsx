1552768da00683b2e0359bb02126c8dcf3641f0c
jacobSalazku <Cooljss10@gmail.com>
2023-09-15 00:14:14 +0200

    styling and ui changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Searchbar from './Search-bar'
+import Logo from './Logo'
 
 const Header = ({ searchText, setSearchText }) => {
   return (
     
     <div className='flex-row header'>
+        <Logo/>
         <Navbar/>
         <Searchbar 
         searchText={searchText} 
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = ({text}) => {
   return (
 
-    <div className='hero'>
+    <div className='hero gradient '>
 
         <h1> {text}</h1>
         
